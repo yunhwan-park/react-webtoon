4390b24e38a5c50d719a787633a89ab6ecd457f5
yunhwan-park <sevenwind@weclover.co.kr>
2021-05-28 11:22:08 +0900

    추가 수정된 파일

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Main from "./container/Main";
+import WebtoonHome from "./container/WebtoonHome";
+import Viewer from "./container/Viewer";
+
+class App extends Component
+{
+  render() {
+    return (
+      <Router>
+        <div>
+          <Route exact path="/" component={Main}></Route>
+          <Route exact path="/webtoon/:webtoonId" component={WebtoonHome}></Route>
+          <Route exact path="/viewer/:episodeId" component={Viewer}></Route>
+        </div>
+      </Router>
+    )
+  }
 }
 
 export default App;
